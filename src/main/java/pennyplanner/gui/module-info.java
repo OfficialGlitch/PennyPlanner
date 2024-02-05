d96fef456ad59e3a8792dda464cfdebeb7dcbdba
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-02-04 19:35:45 -0500

    remove module-info.java

--- src/main/java/pennyplanner/gui/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens pennyplanner.pennyplanner to javafx.fxml;
-    exports pennyplanner.pennyplanner;
-    exports pennyplanner.pennyplanner.gui;
-    opens pennyplanner.pennyplanner.gui to javafx.fxml;
-}
