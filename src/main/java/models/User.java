2a65220dbdde1c08795e74d3a1f6239e20f79603
Glitch <ahmedomaraali@gmail.com>
2024-02-06 02:52:58 -0500

    deleted Id because it is unnecessary

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 @Entity(name = "users")
 @Access(AccessType.PROPERTY)
 public class User {
-    private Long ID;
+
     private String name;
     private String password;
     private String email;
@@ -16,13 +16,7 @@ public class User {
     private ArrayList<Expense> expenseTypes = new ArrayList<>();
     private ArrayList<TimePeriod> history = new ArrayList<>();
 
-    @GeneratedValue
-    public Long getID() {
-        return ID;
-    }
-    public void setID(Long id) {
-        this.ID = id;
-    }
+
 
     @Column
     public String getEmail() {
