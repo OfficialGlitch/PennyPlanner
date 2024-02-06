e96cbf144ce75979503fd1f8fb25254d40eb8f8d
Farzin <aliverdi@my.yorku.ca>
2024-02-06 12:46:46 -0500

    Updated Expense.java with amount and source variable. Created their getters and setters too.

--- src/main/java/money/Expense.java
+++ src/main/java/money/Expense.java
@@ -1,5 +1,22 @@
 package money;
 
 abstract class Expense{
+    private double amount;
+    private String source;
 
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
 }
