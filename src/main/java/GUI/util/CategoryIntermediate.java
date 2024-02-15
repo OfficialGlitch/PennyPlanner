a00b5aa0d5e13adb9e8a314eb9999d3c23f1055c
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-02-14 19:13:18 -0500

    - modify CategoryIntermediate.java - create custom editable treetablecell implementation

--- src/main/java/GUI/util/CategoryIntermediate.java
+++ src/main/java/GUI/util/CategoryIntermediate.java
@@ -1,51 +1,64 @@
 package GUI.util;
 
 import GUI.App;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import models.Category;
 import models.TimePeriod;
 import models.instances.ExpenseInstance;
 import org.hibernate.Session;
-import org.hibernate.annotations.NamedQuery;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CategoryIntermediate implements ExpenseTreeTableItem {
 	
-	public final ObjectProperty<Category> category = new SimpleObjectProperty<>();
+	public final Category category;
 	
 	public CategoryIntermediate(Category cat) {
-		this.category.set(cat);
+		this.category = cat;
 	}
 	
 	private final ObservableList<ExpenseInstance> expenseInstances = FXCollections.observableArrayList();
 	
 	@Override
 	public String name() {
-		return this.category.get().getName();
+		return this.category.getName();
 	}
 	
 	@Override
 	public StringProperty nameProperty() {
-		return category.get().name;
+		return category.name;
 	}
 	
 	@Override
 	public void updateExpenses(TimePeriod timePeriod) {
-		try (Session s = App.s()) {
-			var listy = s.createNamedQuery("getExpenseInstancesForCategory", ExpenseInstance.class)
-				.setParameter("tp", timePeriod.getID())
-				.setParameter("cat", category.get().ID.get())
-				.getResultList();
-			expenseInstances.setAll(listy);
-		}
+		Task<List<ExpenseInstance>> task = new Task<List<ExpenseInstance>>() {
+			@Override
+			protected List<ExpenseInstance> call() throws Exception {
+				List<ExpenseInstance> list;
+				try (Session s = App.sf().openSession()) {
+					list = s.createNamedQuery("getExpenseInstancesForCategory", ExpenseInstance.class)
+						.setParameter("tp", timePeriod.getID())
+						.setParameter("cat", category.ID.get())
+						.getResultList();
+				}
+				return list;
+			}
+		};
+		task.setOnSucceeded((e) -> {
+			expenseInstances.setAll(task.getValue());
+		});
+		ExecutorService es = Executors.newFixedThreadPool(1);
+		es.execute(task);
+		es.shutdown();
 	}
 	
 	@Override
 	public ObservableList<ExpenseInstance> getExpenseInstances(TimePeriod timePeriod) {
-//		updateExpenses(timePeriod);
 		return expenseInstances;
 	}
 	
