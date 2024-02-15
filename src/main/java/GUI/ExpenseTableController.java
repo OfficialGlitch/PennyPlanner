fa9b9074004a559802d39e9521ecf92185189d2e
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-02-14 19:14:52 -0500

    update column cell factories in ExpenseTableController

--- src/main/java/GUI/ExpenseTableController.java
+++ src/main/java/GUI/ExpenseTableController.java
@@ -1,30 +1,41 @@
 package GUI;
 
 import GUI.util.CategoryIntermediate;
+import GUI.util.EditableTreeTableCell;
 import GUI.util.ExpenseTreeTableItem;
 import GUI.util.GenericTableCellFactory;
+import javafx.application.Platform;
 import javafx.beans.InvalidationListener;
 import javafx.beans.WeakInvalidationListener;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
+import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableCell;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.util.Callback;
+import javafx.util.Duration;
+import javafx.util.converter.DefaultStringConverter;
+import javafx.util.converter.DoubleStringConverter;
 import models.Category;
 import models.TimePeriod;
 import models.instances.ExpenseInstance;
 import org.hibernate.Session;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 import java.util.function.Function;
 
-public class ExpenseTableController {
+public class ExpenseTableController implements Initializable {
 	@FXML
 	public TreeTableView<ExpenseTreeTableItem> mainTable;
 	@FXML
@@ -47,10 +58,11 @@ public class ExpenseTableController {
 	
 	private final TreeItem<ExpenseTreeTableItem> troot = new TreeItem<>(null);
 	
+	public static final String NAME_COLUMN = "expnse.name";
+	
 	public void setFields(TimePeriod tp) {
 		this.timePeriod = tp;
-		mainTable.setEditable(true);
-		mainTable.setShowRoot(false);
+//		mainTable.setEditable(true);
 		this.setupTable();
 	}
 	
@@ -72,7 +84,7 @@ public class ExpenseTableController {
 		ObjectProperty<T> prop = new SimpleObjectProperty<>();
 		return (cdf) -> {
 			prop.set(getter.apply(cdf));
-			if(cdf.getValue().getValue() instanceof ExpenseInstance) {
+			if (cdf.getValue().getValue() instanceof ExpenseInstance) {
 				cdf.getTreeTableColumn().setEditable(true);
 			}
 			cdf.getValue().valueProperty().addListener(createListener());
@@ -82,39 +94,84 @@ public class ExpenseTableController {
 	
 	public void setupTable() {
 		updateRows();
+		nameCol.setId(NAME_COLUMN);
+		differenceCol.setEditable(false);
 		nameCol.setCellValueFactory(cellFactory(cdf -> {
-			if(cdf.getValue().getValue() == null) {
+			if (cdf.getValue().getValue() == null) {
 				return "";
 			}
 			return cdf.getValue().getValue().name();
 		}));
 		projectedCostCol.setCellValueFactory(cellFactory(cdf -> {
-			if(cdf.getValue().getValue() == null) {
+			if (cdf.getValue().getValue() == null) {
 				return 0.0d;
 			}
 			return cdf.getValue().getValue().getProjectedCost(timePeriod);
 		}));
 		actualCostCol.setCellValueFactory(cellFactory(cdf -> {
-			if(cdf.getValue().getValue() == null) {
+			if (cdf.getValue().getValue() == null) {
 				return 0.0d;
 			}
 			return cdf.getValue().getValue().getCost(timePeriod);
 		}));
 		differenceCol.setCellValueFactory(cellFactory(cdf -> {
-			if(cdf.getValue().getValue() == null) {
+			if (cdf.getValue().getValue() == null) {
 				return 0.0d;
 			}
 			return cdf.getValue().getValue().difference(timePeriod);
 		}));
-		nameCol.setCellFactory(new GenericTableCellFactory<>(s -> {
-			return s;
-		}));
-		projectedCostCol.setCellFactory(new GenericTableCellFactory<>(s -> String.format("%.2f", s)));
-		actualCostCol.setCellFactory(new GenericTableCellFactory<>(s -> String.format("%.2f", s)));
-		differenceCol.setCellFactory(new GenericTableCellFactory<>(s -> String.format("%.2f", s)));
-		differenceCol.setEditable(false);
+		nameCol.setCellFactory(x -> new EditableTreeTableCell<>(
+			s -> s,
+			new DefaultStringConverter(),
+			(item, value) -> {
+				if (item instanceof ExpenseInstance) {
+					var ei = (ExpenseInstance) item;
+					ei.getExpense().setName(value);
+					mergeItem(ei);
+				} else if (item instanceof CategoryIntermediate) {
+					var ci = (CategoryIntermediate) item;
+					ci.category.setName(value);
+					mergeItem(ci.category);
+				}
+			})
+		);
+		projectedCostCol.setCellFactory(x -> new EditableTreeTableCell<>(
+			s -> String.format("%.2f", s),
+			new DoubleStringConverter(),
+			(item, value) -> {
+				if (item instanceof ExpenseInstance) {
+					var ei = (ExpenseInstance) item;
+					ei.setProjectedCost(value);
+					mergeItem(ei);
+				}
+			})
+		);
+		actualCostCol.setCellFactory(x -> new EditableTreeTableCell<>(
+			s -> String.format("%.2f", s),
+			new DoubleStringConverter(),
+			(item, value) -> {
+				if (item instanceof ExpenseInstance) {
+					var ei = (ExpenseInstance) item;
+					ei.setCost(value);
+					mergeItem(ei);
+				}
+			})
+		);
+		differenceCol.setCellFactory(new GenericTableCellFactory<>(s -> String.format("%.1f", s)));
 		updateRows();
-		mainTable.setRoot(troot);
+	}
+	
+	void mergeItem(Object o) {
+		Platform.runLater(() -> {
+			try (Session s = App.sf().openSession()) {
+				var tx = s.beginTransaction();
+				s.merge(o);
+				tx.commit();
+				updateSubtotal();
+				s.refresh(o);
+			}
+			updateSubtotal();
+		});
 	}
 	
 	public void updateRows() {
@@ -122,7 +179,7 @@ public class ExpenseTableController {
 		
 		List<TreeItem<ExpenseTreeTableItem>> items = new ArrayList<>();
 		
-		try (Session s = App.s()) {
+		try (Session s = App.sf().openSession()) {
 			categories = s.createNamedQuery("getAllCategories", Category.class)
 				.setParameter("user", App.getCurrentUser().getID()).getResultList();
 //			s.refresh(timePeriod);
@@ -130,15 +187,13 @@ public class ExpenseTableController {
 		
 		for (Category cat : categories) {
 			TreeItem<ExpenseTreeTableItem> ti = new TreeItem<>(new CategoryIntermediate(cat));
-			try (Session s = App.s()) {
-				List<ExpenseInstance> eis = s.createNamedQuery("getExpenseInstancesForCategory", ExpenseInstance.class)
-					.setParameter("tp", timePeriod.ID.get()).setParameter("cat", cat.ID.get()).getResultList();
-				List<TreeItem<ExpenseTreeTableItem>> list = new ArrayList<>();
-				eis.forEach(e -> {
-					list.add(new TreeItem<>(e));
-				});
-				ti.getChildren().setAll(list);
-			}
+			List<ExpenseInstance> eis = App.s().createNamedQuery("getExpenseInstancesForCategory", ExpenseInstance.class)
+				.setParameter("tp", timePeriod.ID.get()).setParameter("cat", cat.ID.get()).getResultList();
+			List<TreeItem<ExpenseTreeTableItem>> list = new ArrayList<>();
+			eis.forEach(e -> {
+				list.add(new TreeItem<>(e));
+			});
+			ti.getChildren().setAll(list);
 			items.add(ti);
 		}
 		troot.getChildren().setAll(items);
@@ -151,5 +206,30 @@ public class ExpenseTableController {
 			st += it.getValue().getCost(timePeriod);
 		}
 		subtotal.setText(String.format("%.2f", st));
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		mainTable.setShowRoot(false);
+		mainTable.setRoot(troot);
+		var service = new ScheduledService<Void>() {
+			@Override
+			protected Task<Void> createTask() {
+				return new Task<Void>() {
+					@Override
+					protected Void call() throws Exception {
+						if (mainTable.getRoot() != null)
+							mainTable.getRoot().getChildren().forEach(x -> {
+								x.getValue().updateExpenses(timePeriod);
+							});
+						updateSubtotal();
+						return null;
+					}
+				};
+			}
+		};
+		service.setPeriod(Duration.seconds(1));
+		service.start();
+		
 	}
 }
