5ef6a4bdc7e8134376c2b516650b97e784a8a2e7
Kennie03 <kennie@my.yorku.ca>
2024-02-16 09:30:54 -0500

    New changes

---
+++ src/main/java/Savings/SavingsCalculator.java
@@ -0,0 +1,16 @@
+package Savings;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SavingsCalculator extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+
+	}
+}
