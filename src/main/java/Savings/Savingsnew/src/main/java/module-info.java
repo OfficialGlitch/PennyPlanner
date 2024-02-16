eb4612335f786f67950864bb9633e69154e9090f
Kennie03 <kennie@my.yorku.ca>
2024-02-16 10:05:27 -0500

    New Changes to User

---
+++ src/main/java/Savings/Savingsnew/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module savingscontrol.savingsnew {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens savingscontrol.savingsnew to javafx.fxml;
+    exports savingscontrol.savingsnew;
+}
