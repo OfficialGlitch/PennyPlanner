dfaf3b6e68935719ca02e8a506938e64f12948bc
Glitch <ahmedomaraali@gmail.com>
2024-02-15 21:51:20 -0500

    new test

--- src/main/java/Tests/UserTest.java
+++ src/main/java/Tests/UserTest.java
@@ -1,2 +1,21 @@
-package Tests;public class UserTest {
+package Tests;
+
+import models.money.User;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+
+
+public class UserTest {
+
+	 User user = new User();
+
+
+	@Test
+	public void testSetAndGetUsername() {
+		String username = "testUser";
+		user.setUsername(username);
+		assertEquals(username, user.getUsername());
+	}
+
 }
