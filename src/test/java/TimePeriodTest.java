b53aed5dbf20693dce8d0a065671275f6b875071
Glitch <ahmedomaraali@gmail.com>
2024-02-16 17:59:04 -0500

    added time periods test for testSetAndGetYear

---
+++ src/test/java/TimePeriodTest.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+import java.util.ArrayList;
+import static org.testng.Assert.assertEquals;
+
+import models.TimePeriod;
+
+
+public class TimePeriodTest {
+TimePeriod	timePeriod = new TimePeriod();
+
+	@Test
+	public void testSetAndGetYear() {
+		int year = 2023;
+		timePeriod.setYear(year);
+		assertEquals(year, timePeriod.getYear());
+	}
+
+}
