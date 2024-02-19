3690196dcea964765e2758eb4024617ce9d5dcb9
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-02-19 14:39:22 -0500

    update LoginController

--- src/main/java/GUI/LoginController.java
+++ src/main/java/GUI/LoginController.java
@@ -1,12 +1,20 @@
 package GUI;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import models.money.User;
+import org.mindrot.jbcrypt.BCrypt;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     @FXML
     public AnchorPane loginMessage;
     @FXML
@@ -17,4 +25,25 @@ public class LoginController {
     public Button registerButton;
     @FXML
     public TextField usernameTextField;
+		@FXML
+		public Label errorMessage;
+		
+		public void login(ActionEvent ae) {
+			User user = App.s().createNamedQuery("UserByUsername", User.class).setParameter("username", usernameTextField.getText()).getSingleResultOrNull();
+			if(user == null) {
+				errorMessage.setText("User not found");
+				return;
+			}
+			boolean correctPassword = BCrypt.checkpw(passwordField.getText(), user.getPassword());
+			if(correctPassword) {
+				App.setCurrentUser(user);
+			} else {
+				errorMessage.setText("Wrong username or password");
+				return;
+			}
+		}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+	}
 }
