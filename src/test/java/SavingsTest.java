b0359515ef9b4da7fe1ad7ad1d09b85d7d539efd
Glitch <ahmedomaraali@gmail.com>
2024-02-19 19:24:42 -0500

    Saving test change

--- src/test/java/SavingsTest.java
+++ src/test/java/SavingsTest.java
@@ -1,4 +1,4 @@
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import savings.Savings;
 
@@ -10,18 +10,18 @@ public class SavingsTest {
 		double income1 = 5000.0;
 		double expenses1 = 3000.0;
 		double expectedSavings1 = 2000.0;
-		Assertions.assertEquals(expectedSavings1, Savings.calculateSavings(income1, expenses1), 0.0);
+		assertEquals(expectedSavings1, Savings.calculateSavings(income1, expenses1), 0.0);
 
 		// Test case 2: Income = Expenses
 		double income2 = 4000.0;
 		double expenses2 = 4000.0;
 		double expectedSavings2 = 0.0;
-		Assertions.assertEquals(expectedSavings2, Savings.calculateSavings(income2, expenses2), 0.0);
+		assertEquals(expectedSavings2, Savings.calculateSavings(income2, expenses2), 0.0);
 
 		// Test case 3: Income < Expenses
 		double income3 = 2000.0;
 		double expenses3 = 3000.0;
 		double expectedSavings3 = -1000.0;
-		Assertions.assertEquals(expectedSavings3, Savings.calculateSavings(income3, expenses3), 0.0);
+		assertEquals(expectedSavings3, Savings.calculateSavings(income3, expenses3), 0.0);
 	}
 }
