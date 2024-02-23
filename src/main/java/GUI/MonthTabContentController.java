7d5572eb140a934658abda67a1fcf944cba90f13
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-02-22 21:35:29 -0500

    add balance labels from fxml to MonthTabContentController update totals on field changes

--- src/main/java/GUI/MonthTabContentController.java
+++ src/main/java/GUI/MonthTabContentController.java
@@ -10,7 +10,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import models.TimePeriod;
 
 public class MonthTabContentController implements Initializable
@@ -19,21 +22,51 @@ public class MonthTabContentController implements Initializable
 	private ExpenseTableController expenseTableController;
 	@FXML
 	private IncomeTableController incomeTableController;
-	
+	@FXML
+	private Label actualTotal;
+	@FXML
+	private Text actualTotalCurrency;
+	@FXML
+	private Text diffCurrency;
+	@FXML
+	private Label difference;
+	@FXML
+	private AnchorPane actualWrapper;
+	@FXML
+	private AnchorPane projectedWrapper;
+	@FXML
+	private Text projectedTotalCurrency;
+	@FXML
+	private Label projectedTotal;
+	@FXML
+	private AnchorPane diffWrapper;
 	private TimePeriod timePeriod;
 	private MainPageController mainPageController;
+	
 	public void setFields(TimePeriod ntp, MainPageController mpc) {
 		timePeriod = ntp;
 		mainPageController = mpc;
 		mpc.getTabs().getSelectionModel().selectedItemProperty().addListener((x -> {
-			incomeTableController.setFields(ntp);
-			expenseTableController.setFields(ntp);
+			incomeTableController.setFields(ntp, this);
+			expenseTableController.setFields(ntp, this);
 		}));
-		incomeTableController.setFields(ntp);
-		expenseTableController.setFields(ntp);
+		incomeTableController.setFields(ntp, this);
+		expenseTableController.setFields(ntp, this);
 	}
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 	
+	}
+	public void setBalances(double actual, double projected) {
+		var diffVal = actual - projected;
+		actualTotal.setText(String.format("%.2f", actual));
+		projectedTotal.setText(String.format("%.2f", projected));
+		difference.setText(String.format("%.2f", diffVal));
+		difference.getStyleClass().clear();
+		if(diffVal < 0) {
+			difference.getStyleClass().addAll("text", "danger", "bold");
+		} else if(diffVal > 0) {
+			difference.getStyleClass().addAll("text", "success");
+		}
 	}
 }
