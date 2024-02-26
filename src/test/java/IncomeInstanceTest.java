1ae49d109ab992eaa58b3c685a55536767c4cf04
Glitch <ahmedomaraali@gmail.com>
2024-02-26 18:55:38 -0500

    missing Imports

--- src/test/java/IncomeInstanceTest.java
+++ src/test/java/IncomeInstanceTest.java
@@ -3,5 +3,12 @@ import models.instances.IncomeInstance;
 import models.money.Income;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class IncomeInstanceTest {
+
+
 }
