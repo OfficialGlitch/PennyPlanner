54a42464de35cd80aff063cdd28ffd30a2a9ae73
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-02-26 19:30:44 -0500

    put all pages together within a tabpane

--- src/main/java/GUI/MainPageController.java
+++ src/main/java/GUI/MainPageController.java
@@ -1,7 +1,7 @@
 package GUI;
 
-import javafx.beans.WeakInvalidationListener;
-import javafx.beans.property.ReadOnlyObjectProperty;
+import GUI.Loan.*;
+import javafx.beans.value.WeakChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -10,52 +10,63 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import models.TimePeriod;
 
-import java.io.IOException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.ResourceBundle;
 
-public class MainPageController implements Initializable
-{
+public class MainPageController implements Initializable {
 	@FXML
-	private TabPane tabs;
+	private ExpenseIncomeController expenseIncomePageController;
+	@FXML
+	private LoanController loanCalculatorController;
+	@FXML
+	private PortfolioController portfolioPageController;
+	@FXML
+	private SummaryPageController summaryPageController;
+	@FXML
+	private SavingspageController savingsPageController;
+	@FXML
+	private TabPane months;
+	@FXML
+	private Tab savingsTab;
+	@FXML
+	private Tab portfolioTab;
+	@FXML
+	private Tab loanTab;
+	@FXML
+	private TabPane pageTabs;
+	@FXML
+	private Tab summaryTab;
+	@FXML
+	private Tab expenseIncomeTab;
 	
-	private int year;
-	private final Calendar iCal = Calendar.getInstance();
-	public TabPane getTabs() {return tabs;}
+	private int year = Calendar.getInstance().get(Calendar.YEAR);
 	
-	public void setYear(int nyear) {
-		year = nyear;
-		initTabs();
-	}
-	public void initTabs() {
-		tabs.getTabs().clear();
-		var cal = Calendar.getInstance();
-		for(int i = 0; i < 12; i++) {
-			cal.set(Calendar.MONTH, i);
-			Tab newTab = new Tab(new SimpleDateFormat("MMMM").format(cal.getTime()));
-			URL rootURL = getClass().getResource("MonthTabContent.fxml");
-			FXMLLoader loader = new FXMLLoader(rootURL);
-			MonthTabContentController controller;
-			TimePeriod tp = TimePeriod.generateNewMonth(i + 1, year);
-			Parent root;
-			try {
-				root = loader.load();
-				controller = loader.getController();
-				newTab.setUserData(tp);
-			} catch(IOException e) {
-				throw new RuntimeException("Couldn't load tab: " + e.getMessage());
-			}
-			newTab.setClosable(false);
-			newTab.setContent(root);
-			controller.setFields(tp, this);
-			tabs.getTabs().add(newTab);
-		}
-	}
 	@FXML
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 	
+	}
+	
+	public void setYear(int nyear) {
+		year = nyear;
+	}
+	public void initTabs() {
+		months.getTabs().clear();
+		var cal = Calendar.getInstance();
+		for(int i = 0; i < 12; i++) {
+			cal.set(Calendar.MONTH, i);
+			Tab newTab = new Tab(new SimpleDateFormat("MMMM").format(cal.getTime()));
+			newTab.setClosable(false);
+			months.getTabs().add(newTab);
+		}
+		months.getSelectionModel().selectedIndexProperty().addListener(new WeakChangeListener<Number>((obs, oldValue, newValue) -> {
+			TimePeriod tp = TimePeriod.generateNewMonth((int) newValue + 1, year);
+			expenseIncomePageController.setFields(tp);
+			summaryPageController.setup(tp);
+			portfolioPageController.setup(tp);
+		}));
+		months.getSelectionModel().select(Calendar.getInstance().get(Calendar.MONTH));
 	}
 }
