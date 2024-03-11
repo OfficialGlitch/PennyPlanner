e02db62342849acc616521b324aff0cee84fb45e
Glitch <ahmedomaraali@gmail.com>
2024-03-11 16:29:33 -0400

    added controller for RegistrationController

---
+++ src/main/resources/GUI/RegistrationController.java
@@ -0,0 +1,20 @@
+package GUI;
+
+public class RegistrationController {
+	@FXML
+	public TextField usernameTextField;
+	@FXML
+	public PasswordField passwordField;
+	@FXML
+	public Label errorMessage;
+	@FXML
+	public Button backButton;
+	@FXML
+	public Button registerButton;
+	@FXML
+	public TextField countryTextField;
+	@FXML
+	public TextField nameTextField;
+	@FXML
+	public TextField emailTextField;
+}
