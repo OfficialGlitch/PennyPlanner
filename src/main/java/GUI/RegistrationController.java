e38f09f10509bbdb304d7e8a12011230d200412c
Glitch <ahmedomaraali@gmail.com>
2024-03-11 16:31:23 -0400

    added imports

--- src/main/java/GUI/RegistrationController.java
+++ src/main/java/GUI/RegistrationController.java
@@ -1,5 +1,11 @@
 package GUI;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class RegistrationController {
 	@FXML
 	public TextField usernameTextField;
